5ac5196c611e90448ec47a47f26b4f7d7e2b102b
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-26 20:24:25 -0500

    Modifico pocho

---
+++ src/principal.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class principal {
+
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "hola mundo");
+    }
+}
--- src/prueba1.java
+++ src/prueba1.java
@@ -1,6 +1,3 @@
+
 public class prueba1 {
-    public static void prueba1 (String args) {
-        System.out.println("hola mundo");
-    }
-    
 }
